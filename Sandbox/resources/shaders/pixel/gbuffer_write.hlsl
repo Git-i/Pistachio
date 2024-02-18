c7cddb5772beb9ef997b8067ee10b533638d1b46
Eric Nwogbo <ericis2007@gmail.com>
2024-02-18 21:57:32 +0100

    Started working on rebuilding scene system
    started working on the scene system, would have to restructure the render graph for async compute

--- Sandbox/resources/shaders/pixel/gbuffer_write.hlsl
+++ Sandbox/resources/shaders/pixel/gbuffer_write.hlsl
@@ -1,12 +1,9 @@
-Texture2D albedoTexture : register(t3);
-Texture2D roughnessTexture : register(t4);
-Texture2D metallicTexture : register(t5);
-Texture2D normalTexture : register(t6);
-Texture2D shadowMap[4] : register(t9);
-
-SamplerState my_sampler : register(s0);
-SamplerState ShadowSampler : register(s2);
-cbuffer MaterialData : register(b1)
+Texture2D albedoTexture :    register(t0, space2);
+Texture2D roughnessTexture : register(t1, space2);
+Texture2D metallicTexture :  register(t2, space2);
+Texture2D normalTexture :    register(t3, space2);
+SamplerState my_sampler :    register(s4, space2);
+cbuffer MaterialData : register(b5, space2)
 {
     float4 albedoMultiplier;
     float metallicMultiplier;
@@ -20,9 +17,11 @@ struct PSINTPUT
     float3 Normal : FRAGMENT_NORMAL;
     float2 uv : UV;
     float3 camPos : V_POSITION;
-    float depthViewspace : SM_LAYER;
+    float depthViewSpace : SM_LAYER;
     int numlights : NUM_LIGHTS;
-    float4 lightSpacePositions[16] : LS_POS;
+    float shadowMapSize : SM_SIZE;
+    float4 lightSpacePositions[4] : LS_POS;
+    float4 pos : SV_Position;
 };
 
 struct PSOUTPUT
@@ -31,61 +30,13 @@ struct PSOUTPUT
     float4 normal_rougness : SV_Target1;
     float4 position_metallic : SV_Target2;
     float4 shadow_info : SV_Target3;
-    int id : SV_Target4;
 };
-float Shadow(float3 projCoords, int layer, int index)
-{
-    projCoords.x = (projCoords.x + 1) * 0.25;
-    projCoords.y = (1 - projCoords.y) * 0.25;
-    
-    projCoords.z = 0 + projCoords.z * (1);
-    float currentDepth = projCoords.z;
-    if (currentDepth > 1.0)
-    {
-        return 0.0;
-    }
-    float x[4] = { 0, 0.5, 0, 0.5 };
-    float y[4] = { 0, 0, 0.5, 0.5 };
-    float closestDepth = shadowMap[index].Sample(ShadowSampler, float2(projCoords.x + x[layer], projCoords.y + y[layer])).r;
-    
-    float shadow = currentDepth > closestDepth ? 1.0 : 0.0;
-    return shadow;
-}
 PSOUTPUT main(PSINTPUT input)
 {
     PSOUTPUT pso;
     pso.color = albedoMultiplier * albedoTexture.Sample(my_sampler, input.uv);
-    pso.id = entityID;
     pso.normal_rougness = float4(input.Normal, roughnessMultiplier * roughnessTexture.Sample(my_sampler, input.uv).x);
     pso.position_metallic = float4(input.WorldPos, metallicMultiplier * metallicTexture.Sample(my_sampler, input.uv).x);
-    int shadowMaplayer = 3;
-    float cascadePlaneDistances[4] = { 10.f, 50.f, 70.f, 100.f };
-    if (input.depthViewspace < cascadePlaneDistances[3])
-    {
-        shadowMaplayer = 3;
-    }
-    if (input.depthViewspace < cascadePlaneDistances[2])
-    {
-        shadowMaplayer = 2;
-    }
-    if (input.depthViewspace < cascadePlaneDistances[1])
-    {
-        shadowMaplayer = 1;
-    }
-    if (input.depthViewspace < cascadePlaneDistances[0])
-    {
-        shadowMaplayer = 0;
-    }
-    float3 lightSpacePos = input.lightSpacePositions[shadowMaplayer].xyz / input.lightSpacePositions[shadowMaplayer].w;
-    pso.shadow_info.x = Shadow(lightSpacePos.xyz, shadowMaplayer, 0);
-    
-    lightSpacePos = input.lightSpacePositions[4+shadowMaplayer].xyz / input.lightSpacePositions[4+shadowMaplayer].w;
-    pso.shadow_info.y = Shadow(lightSpacePos.xyz, shadowMaplayer,  1);
-    
-    lightSpacePos = input.lightSpacePositions[8+shadowMaplayer].xyz / input.lightSpacePositions[8+shadowMaplayer].w;
-    pso.shadow_info.z = Shadow(lightSpacePos.xyz, shadowMaplayer, 2);
-    
-    lightSpacePos = input.lightSpacePositions[12+shadowMaplayer].xyz / input.lightSpacePositions[12+shadowMaplayer].w;
-    pso.shadow_info.a = Shadow(lightSpacePos.xyz, shadowMaplayer, 3);
+
     return pso;
 }
---
+++ Sandbox/resources/shaders/vertex/VertexShader.hlsl
@@ -0,0 +1,56 @@
+struct VS_OUT
+{
+    float3 worldpos : WORLD_POSITION;
+	float3 normal : FRAGMENT_NORMAL;
+    float2 UV : UV;
+    float3 viewPos : V_POSITION;
+    float shadowMaplayer : SM_LAYER;
+    int numlights : NUM_LIGHTS;
+    float4 lightSpacePositions[4] : LS_POS;
+    float shadowMapSize : SM_SIZE;
+	float4 position : SV_POSITION;
+};
+
+cbuffer FrameCB : register(b0, space0)
+{
+    float4x4 View;
+    float4x4 InvView;
+    float4x4 Proj;
+    float4x4 InvProj;
+    float4x4 ViewProj;
+    float4x4 InvViewProj;
+    float4 EyePosW;
+    float2 RenderTargetSize;
+    float2 InvRenderTargetSize;
+    float NearZ;
+    float FarZ;
+    float TotalTime;
+    float DeltaTime;
+    matrix lightSpaceMatrix[16];
+    float4 numlights;
+};
+cbuffer ModelCB : register(b1, space1)
+{
+    matrix transform;
+    matrix normalmatrix;
+};
+
+VS_OUT main(float3 pos : POSITION, float3 normal : NORMAL,float2 UV : UV)
+{
+	VS_OUT vso;
+    vso.worldpos = mul(float4(pos, 1.0f), transform).xyz;
+    vso.UV = UV;
+    vso.normal = normalize(mul(normal, (float3x3) normalmatrix));
+    vso.viewPos = EyePosW.xyz;
+    vso.position = mul(mul(float4(pos, 1.0f), transform), ViewProj);
+    vso.numlights = numlights.x;
+    vso.lightSpacePositions[0] = mul(float4(vso.worldpos, 1.f), lightSpaceMatrix[0]);
+    vso.lightSpacePositions[1] = mul(float4(vso.worldpos, 1.f), lightSpaceMatrix[1]);
+    vso.lightSpacePositions[2] = mul(float4(vso.worldpos, 1.f), lightSpaceMatrix[2]);
+    vso.lightSpacePositions[3] = mul(float4(vso.worldpos, 1.f), lightSpaceMatrix[3]);
+    float4 fragPosViewSpace = mul(float4(vso.worldpos, 1.0), View);
+    float depthViewspace = abs(fragPosViewSpace.z);
+    vso.shadowMaplayer = depthViewspace;
+    vso.shadowMapSize = EyePosW.w;
+    return vso;
+}
