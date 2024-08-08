2aa10d5b7ab2f21fcc86964f2519ee20d8a5e55d
Eric Nwogbo <ericis2007@gmail.com>
2024-08-08 22:50:08 +0100

    Fixed ENUM_FLAGS macro

--- subprojects/Pistachio-Engine/src/Pistachio/Core.h
+++ subprojects/Pistachio-Engine/src/Pistachio/Core.h
@@ -11,16 +11,16 @@
 typedef int KeyCode;
 #define ENUM_FLAGS(EnumType)                      \
 inline EnumType operator|(EnumType a, EnumType b) {                             \
-    return static_cast<EnumType>(static_cast<int>(a) | static_cast<int>(b));    \
+    return static_cast<EnumType>(static_cast<std::underlying_type_t<EnumType>>(a) | static_cast<std::underlying_type_t<EnumType>>(b));    \
 }                                                                               \
 inline EnumType operator&(EnumType a, EnumType b) {                             \
-    return static_cast<EnumType>(static_cast<int>(a) & static_cast<int>(b));    \
+    return static_cast<EnumType>(static_cast<std::underlying_type_t<EnumType>>(a) & static_cast<std::underlying_type_t<EnumType>>(b));    \
 }                                                                               \
 inline EnumType operator^(EnumType a, EnumType b) {                             \
-    return static_cast<EnumType>(static_cast<int>(a) ^ static_cast<int>(b));    \
+    return static_cast<EnumType>(static_cast<std::underlying_type_t<EnumType>>(a) ^ static_cast<std::underlying_type_t<EnumType>>(b));    \
 }                                                                               \
 inline EnumType operator~(EnumType a) {                                         \
-    return static_cast<EnumType>(~static_cast<int>(a));                         \
+    return static_cast<EnumType>(~static_cast<std::underlying_type_t<EnumType>>(a));                         \
 }                                                                               \
 inline EnumType& operator|=(EnumType& a, EnumType b) {                         \
     a = a | b;                                                                 \
