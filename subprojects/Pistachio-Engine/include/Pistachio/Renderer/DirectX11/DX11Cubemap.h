05774a9ed28a53505b9290ff437d3f1b7ae37362
Eric Nwogbo <ericis2007@gmail.com>
2024-08-08 22:55:11 +0100

    Removed the "include" folder that was outdated for years

--- subprojects/Pistachio-Engine/include/Pistachio/Renderer/DirectX11/DX11Cubemap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../Core/Error.h"
-namespace Pistachio {
-	class DX11Cubemap {
-	public:
-		static Error Create(int Width, int Height, ID3D11Device* pDevice, ID3D11ShaderResourceView** pSRV, ID3D11Texture2D** rendertargettexture,int miplevels);
-		static void Bind(ID3D11DeviceContext** pCOntext, ID3D11RenderTargetView* pRenderTargetView, ID3D11DepthStencilView* pDSV);
-	};
-}
